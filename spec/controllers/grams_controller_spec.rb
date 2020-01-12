37cd9d522b7bb7233e219f9fcad46dff1a3225b0
ITSONLYJENNI <jennifer.f45@yahoo.com>
2020-01-12 08:42:17 +0000

    adding grams to the database

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -5,6 +5,13 @@ require 'rails_helper'
     it "should successfully show the page" do
       get :index
       expect(response).to have_http_status(:success)
+
+      describe "grams#new action" do
+        it "should successfully show the new form" do
+          get :new
+          expect(response).to have_http_status(:success)
+        end
+      end
     end
   end
 
